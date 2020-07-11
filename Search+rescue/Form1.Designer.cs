16b131eda7e5c08d1730c783ce59788cbf0f6428
Temi-Tory <63935410+Temi-Tory@users.noreply.github.com>
2020-07-11 03:50:43 +0100

    Add project files.

---
+++ Search+rescue/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Search_rescue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Strt_btn = new System.Windows.Forms.Button();
+            this.Abrt_btn = new System.Windows.Forms.Button();
+            this.Adv_btn = new System.Windows.Forms.Button();
+            this.tbx = new System.Windows.Forms.TextBox();
+            this.s_port = new System.IO.Ports.SerialPort(this.components);
+            this.Wlcm_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Strt_btn
+            // 
+            this.Strt_btn.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Strt_btn.ForeColor = System.Drawing.Color.DarkGreen;
+            this.Strt_btn.Location = new System.Drawing.Point(61, 380);
+            this.Strt_btn.Name = "Strt_btn";
+            this.Strt_btn.Size = new System.Drawing.Size(236, 58);
+            this.Strt_btn.TabIndex = 0;
+            this.Strt_btn.Text = "Start";
+            this.Strt_btn.UseVisualStyleBackColor = true;
+            // 
+            // Abrt_btn
+            // 
+            this.Abrt_btn.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Abrt_btn.ForeColor = System.Drawing.Color.DarkRed;
+            this.Abrt_btn.Location = new System.Drawing.Point(355, 380);
+            this.Abrt_btn.Name = "Abrt_btn";
+            this.Abrt_btn.Size = new System.Drawing.Size(142, 58);
+            this.Abrt_btn.TabIndex = 1;
+            this.Abrt_btn.Text = "Abort";
+            this.Abrt_btn.UseVisualStyleBackColor = true;
+            this.Abrt_btn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Adv_btn
+            // 
+            this.Adv_btn.Font = new System.Drawing.Font("Agency FB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Adv_btn.Location = new System.Drawing.Point(713, 403);
+            this.Adv_btn.Name = "Adv_btn";
+            this.Adv_btn.Size = new System.Drawing.Size(75, 35);
+            this.Adv_btn.TabIndex = 2;
+            this.Adv_btn.Text = "Advanced Options";
+            this.Adv_btn.UseVisualStyleBackColor = true;
+            this.Adv_btn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbx
+            // 
+            this.tbx.Location = new System.Drawing.Point(12, 54);
+            this.tbx.Multiline = true;
+            this.tbx.Name = "tbx";
+            this.tbx.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbx.Size = new System.Drawing.Size(713, 290);
+            this.tbx.TabIndex = 3;
+            // 
+            // s_port
+            // 
+            this.s_port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.s_port_DataReceived);
+            // 
+            // Wlcm_lbl
+            // 
+            this.Wlcm_lbl.AutoSize = true;
+            this.Wlcm_lbl.Font = new System.Drawing.Font("Agency FB", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Wlcm_lbl.ForeColor = System.Drawing.Color.SteelBlue;
+            this.Wlcm_lbl.Location = new System.Drawing.Point(28, 9);
+            this.Wlcm_lbl.Name = "Wlcm_lbl";
+            this.Wlcm_lbl.Size = new System.Drawing.Size(318, 42);
+            this.Wlcm_lbl.TabIndex = 4;
+            this.Wlcm_lbl.Text = "Search And Rescue Robot";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Wlcm_lbl);
+            this.Controls.Add(this.tbx);
+            this.Controls.Add(this.Adv_btn);
+            this.Controls.Add(this.Abrt_btn);
+            this.Controls.Add(this.Strt_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Strt_btn;
+        private System.Windows.Forms.Button Abrt_btn;
+        private System.Windows.Forms.Button Adv_btn;
+        private System.Windows.Forms.TextBox tbx;
+        private System.IO.Ports.SerialPort s_port;
+        private System.Windows.Forms.Label Wlcm_lbl;
+    }
+}
+
---
+++ Search+rescue/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Search_rescue
+{
+    public partial class Form1 : Form
+    { string Data_rec;      //declares Data receieved as a global variable
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            s_port.Open();      //this automatically opens a new serial port everytime the form is loaded
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            s_port.Close();   //automatically closes the opened serial port everytime the form closes
+        }
+
+        private void s_port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            Data_rec = s_port.ReadExisting(); // stores recieved data from serieal port as a string
+            this.Invoke(new EventHandler(disp_data));
+            
+        }
+
+        private void disp_data(object sender, EventArgs e)  // defines the method for Disp_data in textbox.
+        {
+           tbx.Text = Data_rec;
+        }
+    }
+}
